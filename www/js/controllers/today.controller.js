e8b8256f7ded047e299350d7887dc431ede8c3d6
Alexander Popelnukhin <popelnukhin@arello-mobile.com>
2015-08-12 12:47:20 +0600

    [feature][ORANGE-27] Today screen

--- www/js/controllers/today.controller.js
+++ www/js/controllers/today.controller.js
@@ -5,35 +5,68 @@
         .module('orange')
         .controller('TodayCtrl', TodayCtrl);
 
-    TodayCtrl.$inject = ['$scope', '$timeout', 'OrangeApi', 'log'];
+    TodayCtrl.$inject = ['$q', '$scope', 'log'];
 
-    function TodayCtrl($scope, $timeout, OrangeApi, log) {
-        $scope.refresh = function () {
-            $timeout(function () {
-                console.log('Rerfreshed');
-                $scope.$broadcast('scroll.refreshComplete');
-            }, 2000);
-        };
+    function TodayCtrl($q, $scope, log) {
+        var vm = this;
 
-        $scope.doLogin = function () {
+        vm.schedule = null;
 
+        vm.medications = null;
 
-            //OrangeApi.patients.getList().then(
-            //    function (patients) {
-            //        console.log(patients.plain());
-            //    },
-            //    function (response) {
-            //        console.log(response);
-            //    }
-            //);
-            //OrangeApi.rxnorm.spelling.post(medication).then(
-            //    function(result) {
-            //        console.log(result.plain());
-            //    },
-            //    function(response) {
-            //        console.log(response);
-            //    }
-            //)
+        vm.filters = [
+            {
+                name: 'Skipped',
+                f: {happened: true, took_medication: false},
+                buttons: '<i class="icon ion-ios-close dark-red"></i>',
+                events: []
+            },
+            {
+                name: 'Taken',
+                f: {happened: true, took_medication: true},
+                buttons: '<i class="icon ion-ios-checkmark green"></i>',
+                events: []
+            },
+            {
+                name: 'Due',
+                f: {happened: false},
+                buttons: '<i class="icon ion-ios-checkmark-outline green"></i> ' +
+                '<i class="icon ion-ios-close-outline dark-red"></i>',
+                events: []
+            }
+        ];
+
+        vm.refresh = refresh;
+
+        refresh();
+
+        function refresh() {
+            var date = moment();
+            var filter = {
+                //start_date: date.format('YYYY-MM-DD'),
+                end_date: date.format('YYYY-MM-DD')
+            };
+            $q.all([log.all('schedule').getList(filter), log.all('medications').getList()])
+                .then(
+                function (data) {
+                    //console.log(schedule);
+                    vm.schedule = data[0].plain();
+                    vm.medications = data[1].plain();
+                    vm.schedule.forEach(function (elem) {
+                        elem.medication = _.find(vm.medications, {id: elem.medication_id});
+                    });
+
+                    vm.filters.forEach(function(filter) {
+                        filter.events = _.filter(vm.schedule, filter.f);
+                    })
+                },
+                function (error) {
+                    console.log(error);
+                }
+            ).finally(
+                function () {
+                    $scope.$broadcast('scroll.refreshComplete');
+                })
         }
     }
 })();
