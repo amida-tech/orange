252dc438f1af5f790440b2cebc5eba17a83d6c7a
Nikita Ekaterinchuk <nekaterinchuk@arello-mobile.com>
2015-08-13 13:29:54 +0600

    [feature][ORANGE-77] Delete Pharmacy

--- www/js/controllers/pharmacies.controller.js
+++ www/js/controllers/pharmacies.controller.js
@@ -5,13 +5,14 @@
         .module('orange')
         .controller('PharmaciesCtrl', PharmaciesCtrl);
 
-    PharmaciesCtrl.$inject = ['$scope', '$ionicLoading', 'log'];
+    PharmaciesCtrl.$inject = ['$scope', '$ionicLoading', '$cordovaDialogs'];
 
-    function PharmaciesCtrl($scope, $ionicLoading, log) {
+    function PharmaciesCtrl($scope, $ionicLoading, $cordovaDialogs) {
         var vm = this;
         vm.pharmaciesPromise = $scope.pharmacies;
         vm.pharmacies = $scope.pharmacies.$object;
         vm.refresh = refresh;
+        vm.remove = remove;
 
         function refresh() {
             vm.pharmacies.getList().then(
@@ -20,6 +21,25 @@
                     vm.pharmacies = pharmacies;
                 }
             );
+        }
+
+        function remove(pharmacy) {
+            $ionicLoading.show({
+                template: 'Deleting...'
+            });
+            pharmacy.remove().then(removeSuccess, removeFailure);
+        }
+
+        function removeSuccess(pharmacy) {
+            $ionicLoading.hide();
+            _.remove(vm.pharmacies, function (item) {
+                return item.id == pharmacy.id;
+            });
+        }
+
+        function removeFailure(error) {
+            $ionicLoading.hide();
+            $cordovaDialogs.alert(error.statusText, 'Error', 'OK');
         }
     }
 })();
