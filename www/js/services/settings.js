15e81fc3d55c53ea64484710ca39f30829c79d43
phobos <phobos@arello-mobile.com>
2015-08-17 17:05:08 +0600

    [artefact][ORANGE-87] fix api url

--- www/js/services/settings.js
+++ www/js/services/settings.js
@@ -4,8 +4,7 @@
     angular
         .module('orange')
         .constant('settings', {
-            //'orangeApiUrl': 'http://orange-api.amida-demo.com/api/v1',
-            'orangeApiUrl': 'http://orange-api.docker.arello-mobile.com/api/v1',
+            'orangeApiUrl': 'http://orange-api.amida-demo.com/api/v1',
             'clientSecret': 'testsecret'
         });
 })();
