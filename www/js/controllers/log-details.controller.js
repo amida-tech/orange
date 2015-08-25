c8ad0a03c1406cf122c9aa6e3d03abc971d4b25f
Nikita Ekaterinchuk <nekaterinchuk@arello-mobile.com>
2015-08-25 15:23:23 +0600

    [feature][ORANGE-53] Call/SMS to patient

--- www/js/controllers/log-details.controller.js
+++ www/js/controllers/log-details.controller.js
@@ -6,13 +6,15 @@
         .controller('LogDetailsCtrl', LogDetailsCtrl);
 
     LogDetailsCtrl.$inject = ['$scope', '$state', '$stateParams', '$ionicPopup', '$ionicLoading',
-                              'LogService'];
+                              '$cordovaActionSheet', '$cordovaSms', 'LogService'];
 
-    function LogDetailsCtrl($scope, $state, $stateParams, $ionicPopup, $ionicLoading, LogService) {
+    function LogDetailsCtrl($scope, $state, $stateParams, $ionicPopup, $ionicLoading, $cordovaActionSheet,
+                            $cordovaSms, LogService) {
 
         var vm = this;
 
         vm.deleteLog = deleteLog;
+        vm.call = call;
 
         vm.currentLog = LogService.setDetailLog($stateParams.id);
         vm.title = vm.currentLog.first_name + ' ' + vm.currentLog.last_name;
@@ -44,6 +46,35 @@
                     }
                 }
             );
+        }
+
+        function call() {
+            $cordovaActionSheet.show({
+                title: 'Select call method',
+                buttonLabels: ['Phone', 'SMS'],
+                addCancelButtonWithLabel: 'Cancel',
+                androidEnableCancelButton: true
+            }).then(function (index) {
+                var phone = vm.currentLog.phone.match(/[\d\+]*/g).join('');
+                if (phone[0] !== '+') {
+                    phone = '+1' + phone;
+                }
+                switch (index) {
+                    case 1:
+                        document.location.href = 'tel:' + phone;
+                        break;
+                    case 2:
+                        $cordovaSms.send(phone, '', {android: {intent: 'INTENT'}}).then(
+                            function (success) {
+                                console.log('Success SMS: ' + success);
+                            },
+                            function (error) {
+                                console.log('Error SMS: ' + error);
+                            }
+                        );
+                        break;
+                }
+            });
         }
     }
 })();
