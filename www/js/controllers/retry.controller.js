9e9f11c0fd299f54c3fb504f52e7f1f7b8464817
phobos <phobos@arello-mobile.com>
2015-08-27 10:50:01 +0600

    [bugfix][ORANGE-93] fix retry on login

--- www/js/controllers/retry.controller.js
+++ www/js/controllers/retry.controller.js
@@ -6,10 +6,10 @@
         .controller('RetryCtrl', RetryCtrl);
 
     RetryCtrl.$inject = [
-        '$rootScope', 'OrangeApi', '$state'
+        '$rootScope', '$state', '$ionicLoading', 'OrangeApi'
     ];
 
-    function RetryCtrl($rootScope, OrangeApi, $state) {
+    function RetryCtrl($rootScope, $state, $ionicLoading, OrangeApi) {
         var vm = this;
 
         var methodsMap = {
@@ -25,6 +25,7 @@
         }
 
         vm.messageText = !$rootScope.parentResponse.status ? 'Connection Lost' : 'API not available yet.';
+        vm.disabled = false;
 
         var url = $rootScope.parentResponse.config.url.replace(OrangeApi.rest.configuration.baseUrl, '');
         var method = methodsMap[$rootScope.parentResponse.config.method];
@@ -33,11 +34,24 @@
 
         vm.retryEvent = function() {
             data = _.isUndefined(data) ? '' : data;
+            vm.disabled = true;
+            $ionicLoading.show({
+               template: 'Retry...'
+            });
+
             OrangeApi.rest.allUrl(url)[method](data).then(function(result) {
                 $state.go($rootScope.currentState);
                 delete $rootScope.currentState;
                 return result;
-            }, $rootScope.promise.reject)
+            }, function(response) {
+                if (response.status != 502 && response.status != 0) {
+                    $state.go($rootScope.currentState);
+                    delete $rootScope.currentState;
+                }
+            }).finally(function(res) {
+                $ionicLoading.hide();
+                vm.disabled = false;
+            })
         }
     }
 })();
