d25aa03379f7839e90449130ddb23f426cfe73c0
phobos <phobos@arello-mobile.com>
2015-08-27 18:20:11 +0600

    [bugfix][ORANGE-112] fix ionic history

--- www/js/controllers/menu.controller.js
+++ www/js/controllers/menu.controller.js
@@ -5,15 +5,24 @@
         .module('orange')
         .controller('MenuCtrl', MenuCtrl);
 
-    MenuCtrl.$inject = ['$scope', '$state', 'Auth', 'patient'];
+    MenuCtrl.$inject = ['$scope', '$state', 'Auth', 'patient', '$ionicHistory', '$timeout'];
 
     /* @ngInject */
-    function MenuCtrl($scope, $state, Auth, patient) {
+    function MenuCtrl($scope, $state, Auth, patient, $ionicHistory, $timeout) {
         $scope.profile = Auth.userInfo();
         $scope.log = patient;
         $scope.patient = patient;
         if (patient == null) {
             $state.go('logs')
+        }
+
+        $scope.fixHistory = function() {
+            $timeout( function() {
+                $ionicHistory.nextViewOptions({
+                    historyRoot: false,
+                    disableAnimate: false
+                });
+            }, 300);
         }
     }
 })();
